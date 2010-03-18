9b08439d9561a1f5399ee2e512007e824be9aebc
xonotic <xonotic@ONE.(none)>
2010-03-18 16:34:21 +0100

    add darkplaces too

--- all-update.sh
+++ all-update.sh
@@ -2,17 +2,23 @@
 
 set -e
 
+repos="
+	data/xonotic-data.pk3dir
+	data/xonotic-maps.pk3dir
+	data/xonotic-music.pk3dir
+	darkplaces
+"
+
 base=`git config remote.origin.url`
 base=${base%/xonotic.git}
-d0=`pwd`/data
-for d in data maps music; do
-	dd="xonotic-$d.pk3dir"
-	if [ -d "$d0/$dd" ]; then
-		cd "$d0/$dd"
-		git config remote.origin.url "$base/$dd.git"
+d0=`pwd`
+for d in $repos; do
+	if [ -d "$d0/$d" ]; then
+		cd "$d0/$d"
+		git config remote.origin.url "$base/${d##*/}.git"
 		git pull
 		cd "$d0"
 	else
-		git clone "$base/$dd.git" "$d0/dd"
+		git clone "$base/${d##*/}.git" "$d0/$d"
 	fi
 done
