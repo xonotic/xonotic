835ee48b40ddb185b6b3583eee1a2917c01df1b9
Rudolf Polzer <divverent@alientrap.org>
2010-06-21 09:45:07 +0200

    JPEG and DDS generator scripts (untested)

--- misc/tools/generate-dds.sh
+++ misc/tools/generate-dds.sh
@@ -2,27 +2,28 @@
 
 set -e
 
-: ${qual:=95}
-
 for X in "$@"; do
 	case "$X" in
 		*.jpg)
 			if [ -n "$scaledown" ]; then
-				mogrify -geometry "$scaledown>" -quality 100 "$X"
+				mogrify -geometry "$scaledown" -quality 100 "$X"
 			fi
-			jpegoptim --strip-all -m$qual "$X"
+			echo "$X has no alpha, converting"
+			nvcompress -bc1 "$X" "${X%.*}.dds"
+			rm -f "$X"
 			;;
 		*.png|*.tga)
 			if [ -n "$scaledown" ]; then
-				mogrify -geometry "$scaledown>" -quality 100 "$X"
+				mogrify -geometry "$scaledown" -quality 100 "$X"
 			fi
 			if convert "$X" -depth 16 RGBA:- | perl -e 'while(read STDIN, $_, 8) { substr($_, 6, 2) eq "\xFF\xFF" or exit 1; ++$pix; } exit not $pix;'; then
 				echo "$X has no alpha, converting"
-				convert "$X" -quality 100 "${X%.*}.jpg"
-				jpegoptim --strip-all -m$qual "${X%.*}.jpg"
+				nvcompress -bc1 "$X" "${X%.*}.dds"
 				rm -f "$X"
 			else
-				echo "$X has alpha, not converting"
+				echo "$X has alpha, converting"
+				nvcompress -alpha -bc3 "$X" "${X%.*}.dds"
+				rm -f "$X"
 			fi
 			;;
 	esac
