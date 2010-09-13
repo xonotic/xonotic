26fb0a13c04234334a3c771ad7bae87f3a0d1638
Rudolf Polzer <divVerent@alientrap.org>
2010-09-13 10:42:06 +0200

    libgmp recompiled to work on older glibc

--- misc/builddeps/dp.linux64/include/gmp.h
+++ misc/builddeps/dp.linux64/include/gmp.h
@@ -2268,7 +2268,7 @@ enum
 
 /* Define CC and CFLAGS which were used to build this version of GMP */
 #define __GMP_CC "gcc -std=gnu99"
-#define __GMP_CFLAGS "-fno-strict-aliasing"
+#define __GMP_CFLAGS "-O2 -pedantic -m64 -mtune=k8 -march=k8"
 
 /* Major version number is the value of __GNU_MP__ too, above and in mp.h. */
 #define __GNU_MP_VERSION 5
