e1e181875ab7334988eae52e5d7a5d1ef015587d
Rudolf Polzer <divVerent@xonotic.org>
2010-11-12 10:09:48 +0100

    add a script "cfgapply" to apply changes from one cfg to another

---
+++ misc/tools/cfgapply.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my %cvar2line = ();
+my @lines = ();
+
+my $first = 1;
+while(<>)
+{
+	chomp;
+	s/\r//g;
+
+	if(/^\s*(?:set\s+|seta\s+)(\S+)/)
+	{
+		if(exists $cvar2line{$1})
+		{
+			$lines[$cvar2line{$1}] = $_;
+		}
+		else
+		{
+			$cvar2line{$1} = scalar @lines;
+			push @lines, $_;
+		}
+	}
+	elsif($first) # only take comments, empty lines from the first config
+	{
+		push @lines, $_;
+	}
+	$first = 0
+		if eof;
+}
+
+print "$_\n" for @lines;
