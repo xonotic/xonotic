e671535ec10fa7fca18d8aa1eed351d3122ba56b
Rudolf Polzer <divverent@alientrap.org>
2010-12-23 13:57:18 +0100

    add a script to tag releases

---
+++ misc/tools/tag-release.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+VER=$1
+
+case "$VER" in
+	'')
+		echo "Need version number as argument"
+		exit 1
+		;;
+	*)
+		;;
+esac
+
+for r in \
+	/home/rpolzer/Games/Xonotic/. \
+	/home/rpolzer/Games/Xonotic/data/xonotic-data.pk3dir \
+	/home/rpolzer/Games/Xonotic/data/xonotic-music.pk3dir \
+	/home/rpolzer/Games/Xonotic/data/xonotic-nexcompat.pk3dir \
+	/home/rpolzer/Games/Xonotic/darkplaces \
+	/home/rpolzer/Games/Xonotic/d0_blind_id \
+	/home/rpolzer/Games/Xonotic/data/xonotic-maps.pk3dir \
+	/home/rpolzer/Games/Xonotic/mediasource \
+	/home/rpolzer/Games/Xonotic/fteqcc
+do
+	cd "$r"
+	git tag -u D276946B -m"version $VER" xonotic-v"$VER"
+done
+
+# excluded repos because not included with releases:
+#	/home/rpolzer/Games/Xonotic/netradiant \
+#	/home/rpolzer/Games/Xonotic/div0-gittools \
