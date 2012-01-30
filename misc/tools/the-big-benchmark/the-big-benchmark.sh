20b4e2dc174ff54ef062c1a67f44f81ce79c4f53
Rudolf Polzer <divverent@alientrap.org>
2012-01-30 17:01:31 +0100

    make quit detection match windows

--- misc/tools/the-big-benchmark/the-big-benchmark.sh
+++ misc/tools/the-big-benchmark/the-big-benchmark.sh
@@ -63,7 +63,7 @@ for e in omg low med normal high ultra ultimate; do
 	rm -f data/benchmark.log
 	echo + "$@" +exec effects-$e.cfg $p > data/engine.log
 	"$@" +exec effects-$e.cfg $p >>data/engine.log 2>&1 || true
-	if grep -xF ']quit' data/engine.log >/dev/null; then
+	if grep '\]quit' data/engine.log >/dev/null; then
 		break
 	fi
 	cat data/engine.log >> data/the-big-benchmark.log
