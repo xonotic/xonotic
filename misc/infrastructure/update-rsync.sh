fc95a413e75897d7f4f80ee10f0449c17e280c38
Rudolf Polzer <divVerent@gmail.com>
2021-07-08 23:52:09 +0200

    Sync the update rsync script.

--- misc/infrastructure/update-rsync.sh
+++ misc/infrastructure/update-rsync.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
-set -x
+set -ex
 
 d=$1
 
 cd /var/rsync/autobuild
 
-for BUILD in '' -low -lowdds; do
+for BUILD in '' -high -mappingsupport; do
+	rm -rf .new
 	mkdir .new
 	cd .new
-	unzip /var/www/autobuild/Xonotic-"$d$BUILD".zip
+	if ! unzip ~/Xonotic-"$d$BUILD".zip; then
+		good=false
+		#for f in ~/Xonotic-"$d$BUILD".zip; do
+		#	unzip "$f" && good=true && break
+		#done
+		$good
+	fi
 	cd Xonotic/data
 	for X in *"$d"*; do
 		pre=${X%$d*}
@@ -17,6 +24,7 @@ for BUILD in '' -low -lowdds; do
 		mv "$X" "$pre"rsync"$post"
 	done
 	cd ../../..
+	rm -rf Xonotic.old
 	mv Xonotic"$BUILD" Xonotic.old || true
 	mv .new/Xonotic Xonotic"$BUILD"
 	rmdir .new
