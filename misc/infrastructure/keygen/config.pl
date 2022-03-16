d84785b6b4506a2cd012f64fbd17814246511f4e
bones_was_here <bones_was_here@xa.org.au>
2022-03-16 21:16:20 +1000

    Update CA files to match srv03 deployment

--- misc/infrastructure/keygen/config.pl
+++ misc/infrastructure/keygen/config.pl
@@ -6,15 +6,20 @@
 		{
 			my ($inc) = @_;
 			return 0 if ($inc >= 0) && check_dnsbl([qr/.*:.*:.*/], [], ['torexit.dan.me.uk', 'aspews.ext.sorbs.net']);
-			return 0 if ($inc >= 0) && check_banlist('http://rm.endoftheinternet.org/~xonotic/bans/?action=list&servers=*');
-			return 0 if check_sql('dbi:mysql:dbname=xonotic_ca', 'xonotic_ca', '************', 'ip', $inc);
+			return 0 if ($inc >= 0) && check_banlist('http://rm.sudo.rm-f.org/~xonotic/bans/?action=list&servers=*');
+			return 0 if check_sql('dbi:mysql:dbname=xonotic-ca', 'xonotic-ca', '************', 'ip', 0.2, 1, 20, 1000, $inc);
 			1;
 		}
 	},
-	15 =>
+	1 =>
 	{
-		name => "Xonotic testing",
-		check => sub { 1; }
+		name => "Xonotic Hub",
+		check => sub
+		{
+			my ($inc) = @_;
+			return 0 if check_ipfiles('/home/xonotic-build/xonotic-release-build/misc/infrastructure/xhub/ips');
+			1;
+		}
 	}
 );
-$default_ca = 15;
+$default_ca = 0;
