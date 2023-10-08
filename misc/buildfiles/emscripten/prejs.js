cba9f389a0df15d5e7111449e7ce955ad4428783
Lock l00p <hakzstudios@gmail.com>
2023-10-08 04:09:39 +0000

    Darkplaces now works

--- misc/buildfiles/emscripten/prejs.js
+++ misc/buildfiles/emscripten/prejs.js
@@ -8,15 +8,15 @@ cmd = function(input){
         currentcmd[i] = input.charCodeAt(i)
 
     }
+    currentcmd[input.length] = 10;
     return 0
 }
-Module['preInit'] = function(){FS.mount("IDBFS",{},"/")}
 Module['arguments'] = ["-xonotic"]
-//pipes output to console
-Module['print'] = function(text){console.log(text); FS.syncfs()}
+Module['print'] = function(text){console.log(text);}
 Module['preRun'] = function(){
+    
     function stdin(){
-    //if current command is default, it just returns 0, code for null
+    /*//if current command is default, it just returns 0, code for null
     if(currentcmd == [0,0,0]){
         return 0
     }
@@ -25,8 +25,10 @@ Module['preRun'] = function(){
     if(cmditerate - 1 > currentcmd.length - 1) {currentcmd = [0,0,0]; return 10}
     return currentcmd[cmditerate - 1]
 
-    }; 
+    */ return 10};
     var stdout = null;
     var stderr = null; 
     FS.init(stdin,stdout,stderr);
+    FS.mkdir('/basedir')
+    FS.mount(IDBFS,{},"/basedir");
 }
