xonotic/xonotic

commit 584edbe3c5a1f0ab189d84d4d7b4a921f8064160

Makefile: improve robustness
Defines all build stages in terms of files generated/required instead of
using PHONY targets.
Ensures `clean` target completes before build targets start and that
no other targets can run while a clean or update target is running.
Works around d0_blind_id autotools flakiness.
Fixes clean targets failing and causing an abort if the d0_blind_id
Makefile doesn't exist.

Makefile
@@ -44,7 +44,7 @@ help:
 	@echo   "  make clean                 Delete engine builds and build objects"
 	@echo
 	@echo   "  make update-stable         Update to the latest stable release via rsync"
-	@echo   "  make update-beta           Update to the latest daily autobuild via rsync"
+	@echo   "  make update-beta           Update to the latest beta autobuild via rsync"
 	@echo
 	@printf "  make server                Compile \e[1m$(SERVER)\e[m\n"
 	@printf "  make client                Compile \e[1m$(CLIENT)\e[m\n"
@@ -56,12 +56,17 @@ ifdef GIT
   $(error $(GIT))
 endif
 
-.EXTRA_PREREQS := $(findstring update-stable,$(MAKECMDGOALS)) $(findstring update-beta,$(MAKECMDGOALS))
+
+# If requested, these targets must always run first:
+.EXTRA_PREREQS := $(filter clean update-stable update-beta, $(MAKECMDGOALS))
+# It makes sense to `clean-sources` after building if requested.
+.NOTPARALLEL: $(.EXTRA_PREREQS) clean-sources
 
 .PHONY: clean-sources
 clean-sources:
 	$(MAKE) -C $(DPSRC) clean
-	$(MAKE) -C $(D0SRC) clean
+	( $(MAKE) -C $(D0SRC) clean || true ) # autotools may not have created the Makefile yet
+clean-sources: .EXTRA_PREREQS =  # prevents circular dependency
 
 .PHONY: clean
 clean: clean-sources
@@ -75,22 +80,28 @@ update-stable:
 update-beta:
 	misc/tools/rsync-updater/update-to-autobuild.sh
 
-$(D0SRC)/Makefile:
-	( cd $(D0SRC) && ./autogen.sh && ./configure --enable-static --disable-shared )
 
-.PHONY: d0_blind_id
-d0_blind_id: $(D0SRC)/Makefile
+$(D0SRC)/.libs/libd0_blind_id.a $(D0SRC)/.libs/libd0_rijndael.a:
+	( cd $(D0SRC) && ./autogen.sh && ./configure --enable-static --disable-shared )
+	$(MAKE) -C $(D0SRC) clean  # ensures missing .a files are created FIXME WORKAROUND
 	$(MAKE) -C $(D0SRC)
 
-.PHONY: server
-server: d0_blind_id
+$(DPSRC)/darkplaces-dedicated: $(D0SRC)/.libs/libd0_blind_id.a $(D0SRC)/.libs/libd0_rijndael.a
 	$(MAKE) -C $(DPSRC) sv-release
-	cp -v $(DPSRC)/darkplaces-dedicated $(SERVER)
+$(SERVER): $(DPSRC)/darkplaces-dedicated
+	cp $(DPSRC)/darkplaces-dedicated $(SERVER)
+
+$(DPSRC)/darkplaces-sdl: $(D0SRC)/.libs/libd0_blind_id.a $(D0SRC)/.libs/libd0_rijndael.a
+	$(MAKE) -C $(DPSRC) sdl-release
+$(CLIENT): $(DPSRC)/darkplaces-sdl
+	cp $(DPSRC)/darkplaces-sdl $(CLIENT)
+
+
+.PHONY: server
+server: $(SERVER)
 
 .PHONY: client
-client: d0_blind_id
-	$(MAKE) -C $(DPSRC) sdl-release
-	cp -v $(DPSRC)/darkplaces-sdl $(CLIENT)
+client: $(CLIENT)
 
 .PHONY: both
 both: client server

commit cd91b50fc45129b170a52c6ebb5539fe3662c53c

Parallel build fixes and minor cleanups
Fixes parallel builds which were broken in
584edbe3c5a1f0ab189d84d4d7b4a921f8064160 by insufficient RTFM.

Enables parallel schroot builds.

Makefile
@@ -55,8 +55,6 @@ endif
 
 # If requested, these targets must always run first:
 .EXTRA_PREREQS := $(filter clean update-stable update-beta, $(MAKECMDGOALS))
-# It makes sense to `clean-sources` after building if requested.
-.NOTPARALLEL: $(.EXTRA_PREREQS) clean-sources
 
 .PHONY: clean-sources
 clean-sources:
@@ -82,12 +80,12 @@ $(D0SRC)/.libs/libd0_blind_id.a $(D0SRC)/.libs/libd0_rijndael.a:
 	$(MAKE) -C $(D0SRC) clean  # ensures missing .a files are created FIXME WORKAROUND
 	$(MAKE) -C $(D0SRC)
 
-$(DPSRC)/darkplaces-dedicated: $(D0SRC)/.libs/libd0_blind_id.a $(D0SRC)/.libs/libd0_rijndael.a
+$(DPSRC)/darkplaces-dedicated: $(D0SRC)/.libs/libd0_blind_id.a
 	$(MAKE) -C $(DPSRC) sv-release
 $(SERVER): $(DPSRC)/darkplaces-dedicated
 	cp $(DPSRC)/darkplaces-dedicated $(SERVER)
 
-$(DPSRC)/darkplaces-sdl: $(D0SRC)/.libs/libd0_blind_id.a $(D0SRC)/.libs/libd0_rijndael.a
+$(DPSRC)/darkplaces-sdl: $(D0SRC)/.libs/libd0_blind_id.a
 	$(MAKE) -C $(DPSRC) sdl-release
 $(CLIENT): $(DPSRC)/darkplaces-sdl
 	cp $(DPSRC)/darkplaces-sdl $(CLIENT)

misc/tools/all/release.subr
@@ -228,7 +228,7 @@ case "$cmd" in
 		esac
 		case " $HOSTS_THAT_ARE_SCHROOT " in
 			*\ $host\ *)
-				schroot="schroot -c $host --"
+				schroot="schroot -c $host --preserve-environment --"
 				;;
 		esac
 		case " $HOSTS_THAT_ARE_MYSELF $HOSTS_THAT_ARE_SCHROOT " in
@@ -307,11 +307,13 @@ case "$cmd" in
 		targetdir=$3
 		verbose mkdir -p "$targetdir"
 		(
-			cd "misc/buildsrc"
+			cd "$d0/misc/buildsrc"
 			verbose ./dlls.sh clean "$buildpath"
 			verbose ./dlls.sh all "$buildpath" "$arch"
 		)
 		verbose cp -v $buildpath/out/$arch/* "$targetdir"
+
+		# SDL2
 		verbose mkdir -p "$d0/misc/builddeps/$arch/"
 		verbose rsync --delete -avP "$buildpath/pkg/$arch/sdl" "$d0/misc/builddeps/$arch/"
 		# PKG_CONFIG HACK: avoids needing to modify sdl2-config