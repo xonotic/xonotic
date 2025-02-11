1f21e4443bf5dfcc6eb47cc5ff5acafd788164e2
NaitLee <naitlee@disroot.org>
2025-02-11 13:02:10 +0800

    fix: clarify comments for cjk font enhancements
    Signed-off-by: NaitLee <naitlee@disroot.org>

--- data/font-notosanscjk.pk3dir/fetch-fonts.sh
+++ data/font-notosanscjk.pk3dir/fetch-fonts.sh
@@ -1,10 +1,14 @@
 #!/bin/env bash
 weight=Medium
+
 mkdir -vp fonts
+
 # fetch from the newest github resources
 for v in SimplifiedChinese,sc TraditionalChinese,tc TraditionalChineseHK,hk Jananese,jp Korean,kr; do
-    curl -o "fonts/NotoSansCJK${v#*,}-${weight}.otf" "https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/OTF/${v%,*}/NotoSansCJK${v#*,}-${weight}.otf"
+    curl -o "fonts/NotoSansCJK${v#*,}-${weight}.otf" \
+        "https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/OTF/${v%,*}/NotoSansCJK${v#*,}-${weight}.otf"
 done
+
 # move jp variant as the default
 mv "fonts/NotoSansCJKjp-${weight}.otf" "fonts/NotoSansCJK-${weight}.otf"
 
